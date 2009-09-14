9f31ff142048a7c5766512fcd3dbbed2b838946a
Janos Kovacs <janos.f.kovacs@nokia.com>
2009-09-15 01:22:17 +0300

    avoid multiple module registration/deregistration by using a hash table

--- src/module-ext.c
+++ src/module-ext.c
@@ -6,10 +6,35 @@
 #include "module-ext.h"
 #include "context.h"
 
+#define HASH_INDEX_BITS    8
+#define HASH_INDEX_GAP     2
+#define HASH_INDEX_MAX     (1 << HASH_INDEX_BITS)
+#define HASH_INDEX_MASK    (HASH_INDEX_MAX - 1)
+#define HASH_TABLE_SIZE    (HASH_INDEX_MAX << HASH_INDEX_GAP)
+#define HASH_TABLE_MASK    (HASH_TABLE_SIZE - 1)
+#define HASH_SEARCH_MAX    HASH_INDEX_MAX
+
+#define HASH_INDEX(i)      (((i) & HASH_INDEX_MASK) << HASH_INDEX_GAP)
+#define HASH_INDEX_NEXT(i) (((i) + 1) & HASH_TABLE_MASK)
+
+
+struct hash_entry {
+    unsigned long      index;
+    struct pa_module  *module;
+};
+
+
+struct hash_entry  hash_table[HASH_TABLE_SIZE];
+
+
 static void handle_module_events(pa_core *, pa_subscription_event_type_t,
                                  uint32_t, void *);
 static void handle_new_module(struct userdata *, struct pa_module *);
 static void handle_removed_module(struct userdata *, unsigned long);
+
+static int hash_add(struct pa_module *);
+static int hash_delete(unsigned long);
+
 
 struct pa_module_evsubscr *pa_module_ext_subscription(struct userdata *u)
 {
@@ -44,8 +69,10 @@ void pa_module_ext_discover(struct userdata *u)
     pa_assert(u->core);
     pa_assert_se((idxset = u->core->modules));
 
-    while ((module = pa_idxset_iterate(idxset, &state, NULL)) != NULL)
+    while ((module = pa_idxset_iterate(idxset, &state, NULL)) != NULL) {
+        hash_add(module);
         handle_new_module(u, module);
+    }
 }
 
 char *pa_module_ext_get_name(struct pa_module *module)
@@ -69,11 +96,18 @@ static void handle_module_events(pa_core *c, pa_subscription_event_type_t t,
         if ((module = pa_idxset_get_by_index(c->modules, idx)) != NULL) {
             name = pa_module_ext_get_name(module);
 
-            handle_new_module(u, module);
+            if (hash_add(module)) {
+                pa_log_debug("new module #%d  '%s'", idx, name);
+                handle_new_module(u, module);
+            }
         }
         break;
         
     case PA_SUBSCRIPTION_EVENT_REMOVE:
+        if (hash_delete(idx)) {
+            pa_log_debug("remove module #%d", idx);
+            handle_removed_module(u, idx);
+        }
         break;
 
     default:
@@ -108,6 +142,46 @@ static void handle_removed_module(struct userdata *u, unsigned long idx)
     }
 }
 
+
+static int hash_add(struct pa_module *module)
+{
+    int hidx = HASH_INDEX(module->index);
+    int i;
+    
+
+    for (i = 0;   i < HASH_SEARCH_MAX;   i++) {
+
+        if (hash_table[hidx].module == NULL) {
+            hash_table[hidx].index  = module->index;
+            hash_table[hidx].module = module;
+            return TRUE;
+        }
+
+        if (hash_table[hidx].module == module)
+            break;
+    }
+
+    return FALSE;
+}
+
+static int hash_delete(unsigned long index)
+{
+    int hidx = HASH_INDEX(index);
+    int i;
+    
+
+    for (i = 0;   i < HASH_SEARCH_MAX;   i++) {
+        if (hash_table[hidx].index == index) {
+            hash_table[hidx].index  = 0;
+            hash_table[hidx].module = NULL;
+            return TRUE;
+        }
+
+        hidx = HASH_INDEX_NEXT(hidx);
+    }
+
+    return FALSE;
+}
 
 
 /*
