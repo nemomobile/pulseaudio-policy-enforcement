77350550fc258379a701a0cde2860d8af4a9b351
Juho Hämäläinen <juho.hamalainen@jolla.com>
2015-07-07 11:31:28 +0300

    [sink-input-ext] Follow mute state and don't override user changes v2. Fixes MER#1154
    Proper implementation of following user mute state. We now track user
    mutes and change actual muting when both policy and user agree on the
    muting.

--- src/sink-input-ext.h
+++ src/sink-input-ext.h
@@ -17,7 +17,8 @@ struct pa_sinp_evsubscr {
     pa_hook_slot    *fixate;
     pa_hook_slot    *put;
     pa_hook_slot    *unlink;
-    pa_hook_slot    *state;
+    pa_hook_slot    *cork_state;
+    pa_hook_slot    *mute_state;
 };
 
 enum pa_sink_input_ext_state {
@@ -32,6 +33,8 @@ struct pa_sink_input_ext {
         int mute;
         uint32_t cork_state;
         bool ignore_cork_state_change;
+        uint32_t mute_state;
+        bool ignore_mute_state_change;
     }                local;     /* local policies */
 };
 
@@ -45,8 +48,9 @@ struct pa_sink_input_ext *pa_sink_input_ext_lookup(struct userdata *,
 int   pa_sink_input_ext_set_policy_group(struct pa_sink_input *, const char *);
 const char *pa_sink_input_ext_get_policy_group(struct pa_sink_input *);
 const char *pa_sink_input_ext_get_name(struct pa_sink_input *);
-int   pa_sink_input_ext_set_volume_limit(struct pa_sink_input *, pa_volume_t);
+int   pa_sink_input_ext_set_volume_limit(struct userdata *u, struct pa_sink_input *, pa_volume_t);
 bool pa_sink_input_ext_cork(struct userdata *u, pa_sink_input *si, bool cork);
+bool pa_sink_input_ext_mute(struct userdata *u, pa_sink_input *si, bool mute);
 
 #endif
 
